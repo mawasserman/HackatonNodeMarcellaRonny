95c504f5815365e63857a3942d0cef03864b7bc9
mawasserman <wasserman.mv@gmail.com>
2024-08-05 19:14:40 +0300

    we are lost

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ const app = express();
 const port = 2222;
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+  console.log(`Server is listening on port ${port}.`);
 });
 
 // get post put patch delete
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
+// app.get("/", (req, res) => {
+//     res.send("Hello World!"); 
+    
+//   });
+
+app.get('/api/products', (req, res) => {
+    res.json([
+        { name: 'iPhone', price: 800 },
+        { name: 'iPad', price: 650 },
+        { name: 'iWatch', price: 750 }
+    ])
+});
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/aboutme', (req, res) => res.send('I love coding!'))
+app.get('/tutorial', (req, res) => res.send('Tutorial about express!'))
 
 // http ???
 
 app.post("/about", (req, res) => {
     res.send("About me");
   });
-  
